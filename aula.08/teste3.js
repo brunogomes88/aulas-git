65cd52ced76179a6e04c2169f8f2c386abb46ddc
bruno gomes de souza <brunosouzaoficialbh@gmail.com>
2022-02-27 22:06:05 -0300

    meu segundo projeto

---
+++ aula.08/teste3.js
@@ -0,0 +1,14 @@
+let nome = "Bruno"
+let altura = 1.65 
+let peso = "86kg" 
+const profissao = "programador"
+//console.log (nome + ',' + altura + ',' + peso + ',' + profissao)
+ 
+
+function idade(){
+    if(idade >= 18)
+    console.log("pode tirar CNH")
+    else{console.log("Ops, você é de menor")}
+
+}
+idade(17)
