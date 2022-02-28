5926a6997cc6aee649068bdc7bd7d8ee38bc22c8
bruno gomes de souza <brunosouzaoficialbh@gmail.com>
2022-02-28 18:56:32 -0300

    meu primeiro commit

--- aula.08/teste3.js
+++ aula.08/teste3.js
@@ -8,7 +8,7 @@ const profissao = "programador"
 function idade(){
     if(idade >= 18)
     console.log("pode tirar CNH")
-    //else{console.log("Ops, você é de menor")}
+    else{console.log("Ops, você é de menor")}
 
 }
 idade(17)
@@ -17,3 +17,5 @@ tabuada = 8
 for(let i=1; i<=10; i++){
   console.log('8 x ' + i + ' = ' + 8*i)
 }
+
+
